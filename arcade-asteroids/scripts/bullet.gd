12a6d3b0acaaf2af2ced61fb2f10203ddd45661d
bakudas <bakudas@gmail.com>
2026-01-26 22:13:53 -0300

    setup bullet and movement

---
+++ arcade-asteroids/scripts/bullet.gd
@@ -0,0 +1,23 @@
+class_name Bullet extends Area2D
+
+@export var speed: int = 1000
+var velocity: Vector2 = Vector2.ZERO
+
+
+func start(_transform: Transform2D) -> void:
+	transform = _transform
+	velocity = transform.x * speed
+
+
+func _process(delta: float) -> void:
+	position += velocity * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("rocks") and body.has_method("explode"):
+		body.explode()
+		queue_free()
